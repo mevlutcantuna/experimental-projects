b3e634781f8d59b3a0ad05de70b12c79436de103
Mevlüt Can Tuna <mttuna90@gmail.com>
2023-09-11 17:07:16 +0300

    gitignore file changed

--- dopt/src/components/Onboarding/components/DoptTourItemDialog.tsx
+++ dopt/src/components/Onboarding/components/DoptTourItemDialog.tsx
@@ -2,7 +2,7 @@ interface DoptTourItemProps {
   blockId: string;
 }
 const DoptTourItemDialog = ({ blockId }: DoptTourItemProps) => {
-  return <div>{blockId}</div>;
+  return <div className="w-screen h-screen relative">{blockId}</div>;
 };
 
 export default DoptTourItemDialog;
